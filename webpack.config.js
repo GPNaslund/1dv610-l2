f7f502db9bece50ad57ee9826097dbd2e2821e95
Gustav <gpnaslund@gmail.com>
2023-10-04 23:19:01 +0200

    Starts to build the application

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    fallback: {
+      "fs": false,
+      "path": false,
+      "os": false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-private-methods']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development'
+};
